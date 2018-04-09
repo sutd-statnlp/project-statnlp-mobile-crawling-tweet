12e09b571ad58fe052c329917c0a3fff93937873
Ai Nguyen <ainguyenkaka@gmail.com>
2018-04-09 16:01:24 +0800

    init

---
+++ www/js/index.js
@@ -0,0 +1,13 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+
+    }
+
+};
+
+app.initialize();
